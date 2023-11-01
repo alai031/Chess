607d33e86b141f417eb7aa61159a74a9bbabebc4
Auberon <alai031@ucr.edu>
2023-10-31 23:42:13 -0700

    Added rough draft of Chess where player can play against an AI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useState} from 'react';
+import {Chessboard} from 'react-chessboard';
+import {Chess} from 'chess.js';
 
 function App() {
+
+  const [game, setGame] = useState(new Chess());
+
+  const makeMove = (move) => {
+    game.move(move);
+    setGame(new Chess(game.fen()))
+  }
+
+  //Movement of computer
+  function makeRandomMove(){
+    const possibleMoves = game.moves();
+
+    //exit if the game is over
+
+    if (game.isGameOver() || game.isDraw() || possibleMoves.length === 0)
+      return;
+
+    //select random move
+
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+
+    makeMove(possibleMoves[randomIndex]);
+  }
+
+  //Perform an action when a piece is dropped by a user
+
+  function onDrop(source, target){
+    const move = makeMove({
+      from: source,
+      to: target,
+      promotion: "q", //always promote to a queen for example simplicity
+    });
+
+    // illegal move
+    if (move === null)
+      return false;
+
+    //valid move
+    setTimeout(makeRandomMove, 200);
+    return true;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+       <Chessboard
+        position = {game.fen()}
+        onPieceDrop = {onDrop}
+       />
     </div>
   );
 }
